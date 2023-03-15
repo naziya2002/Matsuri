02b65bca026fc3912816e70df11b5637714fa91f
unknown <naziyaanjum220@gmail.com>
2023-03-15 19:31:49 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -49,17 +49,17 @@ function subtract3(){
 
 
 
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+// menu.onclick = () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
 
-};
+// };
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-};
+// window.onscroll = () =>{
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// };
 
